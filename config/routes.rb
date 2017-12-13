0ebf6d0ea0437e41fa98fface87199fd9765cb3b
Anna Sedlar <asedlar@bignerdranch.com>
2017-12-13 12:47:24 -0500

    +bootstrap and linking views to routes in nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :articles
 
-  root 'welcome#index'
+  root 'welcome#index', as: 'home'
+
+  get 'about' => 'pages#about', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
