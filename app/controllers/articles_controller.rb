6e08818e013c14b56ad1b1f2d38d80c94d508870
Anna Sedlar <asedlar@bignerdranch.com>
2017-12-13 13:36:18 -0500

    fix delete path from going to edit path. Robby++

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
 
     def destroy
         @article = Article.find(params[:id])
-        @post.destroy
+        @article.destroy
         redirect_to articles_path        
     end
 
